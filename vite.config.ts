739d1f2a65c0925f0b41d44465c1b394ef6ce9b4
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2024-11-01 12:42:55 -0400

    fix netlify port

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4173,
+    port: 8888,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
